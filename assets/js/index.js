9ef047f90407d67c452e6348294f1f8a591ff729
Colibridev <54209311+Fifaliana-tech@users.noreply.github.com>
2022-05-07 19:58:18 +0300

    Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,39 @@
+var home = document.getElementById('home');
+var game = document.getElementById('game');
+
+function menu(clicked_id){
+    if(clicked_id === "home"){
+        home.style.textDecoration = 'underline';
+        home.style.cursor = 'pointer';
+        home.style.fontWeight = '800';
+        game.style.fontWeight = '100';
+        game.style.textDecoration = 'none';
+        console.log('clicH');
+    } else {
+        game.style.textDecoration = 'underline';
+        game.style.cursor = 'pointer';
+        game.style.fontWeight = '800';
+        home.style.fontWeight = '100';
+        home.style.textDecoration = 'none';
+        console.log('clicG');
+    }
+}
+console.log(home);
+
+// scroll
+// $("mouse").scroll(function(){
+//     console.log("Mouse scrolled");
+// });
+window.addEventListener("scroll", (event) => {
+    let mouse = document.getElementById('mouse');
+    let scrollY = this.scrollY;
+
+//QUIZZ
+var countChecked = function() {
+    var n = $( "input:checked" ).length;
+    $( ".score" ).text( n + (n === 1 ? " is" : " are") + " checked!" );
+  };
+  countChecked();
+   
+  $( "input[type=checkbox]" ).on( "click", countChecked );
+
