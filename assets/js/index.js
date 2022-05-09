059c044a274c846e5be52b073fadb7b6e0644cb1
Colibridev <54209311+Fifaliana-tech@users.noreply.github.com>
2022-05-09 20:00:50 +0300

    bande annonce

--- assets/js/index.js
+++ assets/js/index.js
@@ -115,3 +115,39 @@ function showSlides() {
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
 showSlides();
+
+//Multimédia
+// window.addEventListener('scroll',() => {
+//     let slideIndex = 1;
+//     showSlides(slideIndex);
+    
+//     function plusSlides(n) {
+//       showSlides(slideIndex += n);
+//     }
+    
+//     function currentSlide(n) {
+//       showSlides(slideIndex = n);
+//     }
+    
+//     function showSlides(n) {
+//       let i;
+//       let slides = document.getElementsByClassName("mySlides");
+//       let dots = document.getElementsByClassName("dot");
+//       if (n > slides.length) {slideIndex = 1}    
+//       if (n < 1) {slideIndex = slides.length}
+//       for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";  
+//       }
+//       for (i = 0; i < dots.length; i++) {
+//         dots[i].className = dots[i].className.replace(" active", "");
+//       }
+//       slides[slideIndex-1].style.display = "block";  
+//       dots[slideIndex-1].className += " active";
+//     }
+// });
+// BANDE ANNONCE
+var vid = document.getElementById("myVideo"); 
+
+function playVid() { 
+  vid.play(); 
+} ;
